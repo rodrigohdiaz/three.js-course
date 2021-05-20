7b1e99cdc8dd50187eeca793c8b6f0b0adcb25cd
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-20 14:15:21 +1200

    started textures

--- excercise folder/11-textures/src/script.js
+++ excercise folder/11-textures/src/script.js
@@ -15,6 +15,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // }
 // image.src = '/textures/door/color.jpg'
 const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () =>
+{
+    
+}
+
 const textureLoader = new THREE.TextureLoader(loadingManager)
 const texture = textureLoader.load('/textures/door/color.jpg')
 
