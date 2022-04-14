79a495b0b1e0f73dbb688fa5c77a0327e94ce6a8
Rodrigo Diaz <rodrigohdiaz@gmail.com>
2022-04-14 18:51:39 +1200

    finish setting up, adds classes and debug scene

--- excercise folder/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ excercise folder/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -4,6 +4,11 @@ import Time from "./Utils/Time.js"
 import Camera from "./Camera.js"
 import Renderer from "./Renderer.js"
 import World from "./World/World.js"
+import Resources from './Utils/Resources.js'
+import Debug from './Utils/Debug.js'
+import sources from "./sources.js"
+
+
 
 let instance = null
 
@@ -25,9 +30,11 @@ export default class Experience
         this.canvas = canvas
         
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
@@ -55,6 +62,38 @@ export default class Experience
     update()
     {
         this.camera.update()
+        this.world.update()
         this.renderer.update()
+    }
+
+    destroy()
+    {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+                    
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if(this.debug.active)
+            this.debug.ui.destroy()
     }
 }
--- excercise folder/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ excercise folder/26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,5 +1,7 @@
-import * as THREE from "three"
 import Experience from "../Experience.js"
+import Environment from "./Environment.js"
+import Floor from "./Floor.js"
+import Fox from "./Fox.js"
 
 export default class World
 {
@@ -7,13 +9,21 @@ export default class World
     {
         this.experience = new Experience()
         this.scene = this.experience.scene
+        this.resources = this.experience.resources
         
 
-        // Test mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({ wireframe: true})
-        )
-        this.scene.add(testMesh)
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            this.floor = new Floor()
+            this.fox = new Fox()
+            this.environment = new Environment()
+        })
+    }
+    
+    update()
+    {
+        if(this.fox)
+        this.fox.update()
     }
 }
